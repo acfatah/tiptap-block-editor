cb6f056e3e17bbd14eeba5dceab7221cf5f3eb56
Achmad F. Ibrahim <acfatah@gmail.com>
2026-02-27 16:05:01 +0800

    chore: update dropdown menu component

--- src/components/ui/dropdown-menu/DropdownMenuContent.vue
+++ src/components/ui/dropdown-menu/DropdownMenuContent.vue
@@ -10,14 +10,9 @@ import { cn } from '@/lib/utils'
 
 interface Props extends MenuContentProps {
   class?: HTMLAttributes['class']
-  align?: 'start' | 'center' | 'end'
-  side?: 'top' | 'right' | 'bottom' | 'left'
 }
 
-const props = withDefaults(defineProps<Props>(), {
-  side: 'right',
-})
-
+const props = defineProps<Props>()
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardPropsEmits(delegatedProps)
 </script>
@@ -29,7 +24,6 @@ const forwardedProps = useForwardPropsEmits(delegatedProps)
         v-bind="forwardedProps"
         data-scope="dropdown-menu"
         data-part="content"
-        :data-side="props.side"
         :class="cn(
           `
             z-50 max-h-(--available-height) min-w-32 origin-(--transform-origin) overflow-x-hidden
@@ -37,10 +31,10 @@ const forwardedProps = useForwardPropsEmits(delegatedProps)
             outline-none
           `,
           `
-            data-[side=bottom]:slide-in-from-top-2
-            data-[side=left]:slide-in-from-right-2
-            data-[side=right]:slide-in-from-left-2
-            data-[side=top]:slide-in-from-bottom-2
+            data-[placement^=bottom]:slide-in-from-top-1.5 data-[placement^=bottom]:-mt-1.5
+            data-[placement^=left]:-mr-1.5 data-[placement^=left]:slide-in-from-right-2
+            data-[placement^=right]:-ml-1.5 data-[placement^=right]:slide-in-from-left-2
+            data-[placement^=top]:-mb-1.5 data-[placement^=top]:slide-in-from-bottom-2
             data-[state=closed]:animate-out data-[state=closed]:fade-out-0
             data-[state=closed]:zoom-out-95
             data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95
--- src/components/ui/dropdown-menu/DropdownMenuRoot.vue
+++ src/components/ui/dropdown-menu/DropdownMenuRoot.vue
@@ -3,21 +3,51 @@ import type { MenuRootEmits, MenuRootProps } from '@ark-ui/vue/menu'
 import type { HTMLAttributes } from 'vue'
 
 import { Menu } from '@ark-ui/vue/menu'
+import { reactiveOmit } from '@vueuse/core'
+import { computed } from 'vue'
 
 import { useForwardPropsEmits } from '@/composables/useForwardPropsEmits'
 
 interface Props extends MenuRootProps {
   class?: HTMLAttributes['class']
+  align?: 'start' | 'center' | 'end'
+  side?: 'top' | 'right' | 'bottom' | 'left'
 }
 
-const props = defineProps<Props>()
+type Placement = NonNullable<
+  NonNullable<MenuRootProps['positioning']>['placement']
+>
+
+const props = withDefaults(defineProps<Props>(), {
+  align: 'start',
+  side: 'bottom',
+})
+
 const emit = defineEmits<MenuRootEmits>()
-const forwardedProps = useForwardPropsEmits(props, emit)
+const delegatedProps = reactiveOmit(props, [
+  'class',
+  'align',
+  'positioning',
+  'side',
+])
+const forwardedProps = useForwardPropsEmits(delegatedProps, emit)
+
+const positioning = computed(() => {
+  const placement = (props.align === 'center'
+    ? props.side
+    : `${props.side}-${props.align}`) as Placement
+
+  return {
+    ...props.positioning,
+    placement: props.positioning?.placement ?? placement,
+  }
+})
 </script>
 
 <template>
   <Menu.Root
     v-bind="forwardedProps"
+    :positioning="positioning"
   >
     <slot />
   </Menu.Root>
